cfac0ce4558f4b8c79c17a2af695d6bd44ea0a27
Maddy Reid <maddy.reid.21@gmail.com>
2014-01-07 10:40:38 +1100

    Trying to get creation script to work

--- create_db.py
+++ create_db.py
@@ -1,8 +1,10 @@
 
-from application import db
-from application.models import Status
+from application import app, db
+from application.models import *
 
 #print Status
+db.drop_all()
+db.init_app(app)
 db.create_all()
 
 sopen = Status("Open")
@@ -10,8 +12,11 @@ sorder = Status("Ordering")
 spickup = Status("Pickup")
 sclosed = Status("Closed")
 
+inituser = User("Maddy")
+
 db.session.add(sopen)
 db.session.add(sorder)
 db.session.add(spickup)
 db.session.add(sclosed)
+db.session.add(inituser)
 db.session.commit()
