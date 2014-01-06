34dfd7957bd65d9ff0cad925b8ac8a877056d5f1
Maddy Reid <maddy.reid.21@gmail.com>
2014-01-07 00:01:05 +1100

    Messing with config

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
+import os
 
 DEBUG = True
 CSFR_ENABLED = True
 SECRET_KEY = "HyP0oHYnYeqv47uXohfvOkiv"
-#SQLALCHEMY_DATABASE_URI = "mysql+mysqldb://flask:@localhost/coffeerun"
 if os.environ.get('DATABASE_URL') is None:
-    SQLALCHEMY_DATABASE_URI = "mysql+mysqldb://flask:@localhost/coffeerun"
+    #SQLALCHEMY_DATABASE_URI = "mysql+mysqldb://flask:@localhost/coffeerun"
+    SQLALCHEMY_DATABASE_URL = "postgres://pzjpocurmvfdee:_J2pg9gvP0K5SJeMPHjUkERt2J@ec2-54-197-250-52.compute-1.amazonaws.com:5432/d4bnpmfglihmg0"
 else:
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
