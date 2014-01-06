7956a12bd62b6271fd5308be33160d600075c4be
Maddy Reid <maddy.reid.21@gmail.com>
2014-01-06 23:20:49 +1100

    Fixed database URL

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -b 127.0.0.1:5000 run:app
+web: gunicorn run-heroku:app
--- config.py
+++ config.py
@@ -2,4 +2,8 @@
 DEBUG = True
 CSFR_ENABLED = True
 SECRET_KEY = "HyP0oHYnYeqv47uXohfvOkiv"
-SQLALCHEMY_DATABASE_URI = "mysql+mysqldb://flask:@localhost/coffeerun"
+#SQLALCHEMY_DATABASE_URI = "mysql+mysqldb://flask:@localhost/coffeerun"
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = "mysql+mysqldb://flask:@localhost/coffeerun"
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
