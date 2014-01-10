da712fe448fc19524b90b296b826396d98228cd1
Maddy Reid <maddy.reid.21@gmail.com>
2014-01-10 15:38:30 +1100

    Timezone display maybe?

--- application/models.py
+++ application/models.py
@@ -65,7 +65,7 @@ class Run(db.Model):
         return self.deadline.strftime("%I:%M %p %a %d %b")
 
     def readmodified(self):
-        return self.modified.strftime("%I:%M %p %a %d %b")
+        return self.modified.astimezone("Australia/Sydney").strftime("%I:%M %p %a %d %b")
 
     def jsondatetime(self, arg):
         tformat = "%Y-%m-%d %H:%M:%S"
