990f96b312f2739953b220da709c3e7c2a3b4816
Maddy Reid <maddy.reid.21@gmail.com>
2015-01-06 09:36:20 +1100

    get rid of keys...

--- config.py
+++ config.py
@@ -6,11 +6,11 @@ import os
 
 class Config(object):
     CSFR_ENABLED = True
-    SECRET_KEY = "HyP0oHYnYeqv47uXohfvOkiv"
+    SECRET_KEY = "fake"
     MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = "465"
     MAIL_USERNAME = "ncsscoffeerun@gmail.com"
-    MAIL_PASSWORD = "kpTMnBNZn06z"
+    MAIL_PASSWORD = "fake"
     MAIL_DEFAULT_SENDER = ("NCSS Coffeebot", "ncsscoffeerun@gmail.com")
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
@@ -27,7 +27,7 @@ class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = "sqlite:///coffeerun-prod.db"
     DEBUG = True
     if os.environ.get('DATABASE_URL') is None:
-        SQLALCHEMY_DATABASE_URL = "postgres://pzjpocurmvfdee:_J2pg9gvP0K5SJeMPHjUkERt2J@ec2-54-197-250-52.compute-1.amazonaws.com:5432/d4bnpmfglihmg0"
+        SQLALCHEMY_DATABASE_URI = "INSERT KEY HERE"
     else:
         SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-    API_KEY = "AIzaSyDdtXLXJWPJS9bEay-nq0QsAvFxHMvGw3U"
+    API_KEY = "fake"
