07cb21cc84da50a28a44aaa6b312477308919eb6
Maddy Reid <maddy.reid.21@gmail.com>
2016-01-03 18:58:37 +1100

    Original create script

--- Procfile
+++ Procfile
@@ -1,5 +1,6 @@
 web: gunicorn run-heroku:app
 init: python manage.py db init
+createdb: python create_db.py
 upgrade: python manage.py db upgrade
 celery: celery -A application.celery worker -B --loglevel=info
 worker: python coffeebot.py
--- create_db.py
+++ create_db.py
@@ -1,18 +1,11 @@
 
-from application import app, db
+from application import db
 from application.models import *
 
-from migrate.versioning import api
-import os.path
 
 db.drop_all()
 db.init_app(app)
 db.create_all()
-if not os.path.exists(app.config["SQLALCHEMY_MIGRATE_REPO"]):
-    api.create(app.config["SQLALCHEMY_MIGRATE_REPO"], 'database repository')
-    api.version_control(app.config["SQLALCHEMY_DATABASE_URI"], app.config["SQLALCHEMY_MIGRATE_REPO"])
-else:
-    api.version_control(app.config["SQLALCHEMY_DATABASE_URI"], app.config["SQLALCHEMY_MIGRATE_REPO"], api.version(app.config["SQLALCHEMY_MIGRATE_REPO"]))
 
 db.session.add(Cafe('Toby\'s Estate', 'City Rd'))
 db.session.add(Cafe('Campos', 'Newtown'))
