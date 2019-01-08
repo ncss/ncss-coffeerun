e0cab9f2077893f85a14ac8924bc59b2efbee3ee
tsukasa-au <tsukasa-au@users.noreply.github.com>
2019-01-09 01:56:06 +1100

    monitoring: Add sentry.io monitoring. (#19)
    * monitoring: Add sentry.io monitoring.
    
    In theory this will give me somewhere to see the exceptions that happen.

--- coffeebot.py
+++ coffeebot.py
@@ -14,6 +14,8 @@ import coffeespecs
 
 import flask_babel
 
+import sentry_sdk
+
 from slackclient import SlackClient
 
 import utils
@@ -318,6 +320,7 @@ def main():
 
 
 if __name__ == '__main__':
+    sentry_sdk.init("https://a4ba88dce1234b4ea13f5c3111a67efa@sentry.io/1366725")
     logging.basicConfig(level=logging.DEBUG)
 
     # FIXME: This is a hack... But I can't think of anything better.
--- run-heroku.py
+++ run-heroku.py
@@ -1,1 +1,5 @@
 from application import app  # noqa: F401
+
+import sentry_sdk
+
+sentry_sdk.init("https://a4ba88dce1234b4ea13f5c3111a67efa@sentry.io/1366725")
